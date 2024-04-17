f39e83fed2147e345e9bf1319a7552ca26912190
Bryan Kongnyu <144625899+bryancods@users.noreply.github.com>
2024-04-17 20:11:08 +0000

    more bootstrap

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
+// main.js/ts
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
 import { createBootstrap } from 'bootstrap-vue-next'
-import fixtures from './fixtures.json';
+
+// Add the necessary CSS
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
-
-app.use(createBootstrap())
-app.use(router)
-app.config.globalProperties.$fixtures = fixtures;
-app.config.globalProperties.baseDir = import.meta.env.BASE_URL;
-
+app.use(createBootstrap()) // Important
 app.mount('#app')
