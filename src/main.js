8e5d36d486efb821f1266574059571aa373a101a
Bryan Kongnyu <144625899+bryancods@users.noreply.github.com>
2024-04-17 20:04:35 +0000

    bootstrap added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createBootstrap } from 'bootstrap-vue-next'
+import fixtures from './fixtures.json';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
 
+app.use(createBootstrap())
 app.use(router)
+app.config.globalProperties.$fixtures = fixtures;
+app.config.globalProperties.baseDir = import.meta.env.BASE_URL;
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
