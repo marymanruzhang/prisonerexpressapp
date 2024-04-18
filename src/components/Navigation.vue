58e6ffc3bdd5fb13633c36b174a06d8f9f53d26f
graceeylee <143830712+graceeylee@users.noreply.github.com>
2024-04-18 15:30:13 +0000

    minor b-col changes

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,12 +7,16 @@
                         <BImg src="./icons/mag_glass_icon.png" fluid />
                     </b-nav-item>
                 </b-col>
-                <b-nav-item>
-                    <BImg src="./icons/home_icon.png" fluid />
-                </b-nav-item>
-                <b-nav-item>
-                    <BImg src="./icons/info_icon.png" fluid />
-                </b-nav-item>
+                <b-col>
+                    <b-nav-item>
+                        <BImg src="./icons/home_icon.png" fluid />
+                    </b-nav-item>
+                </b-col>
+                <b-col>
+                    <b-nav-item>
+                        <BImg src="./icons/info_icon.png" fluid />
+                    </b-nav-item>
+                </b-col>
             </b-navbar>
         </b-row>
     </b-container>
