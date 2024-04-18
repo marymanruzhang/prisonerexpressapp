7abda2d7c7c3ffa50c972eda72de86dc6fc90b74
marymanruzhang <156651928+marymanruzhang@users.noreply.github.com>
2024-04-18 15:06:52 +0000

    Accordian working

---
+++ src/views/FAQ.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+<h1>Frequently Asked Questions</h1>
+<p>Below are frequently asked questions</p>
+<BAccordion>
+  <BAccordionItem title="Accordion Item #1" visible>
+    <strong>This is the first item's accordion body.</strong> Hello!!
+  </BAccordionItem>
+  <BAccordionItem title="Accordion Item #2">
+    <strong>This is the second item's accordion body.</strong> Hello!!!
+  </BAccordionItem>
+  <BAccordionItem title="Accordion Item #3">
+    <strong>This is the third item's accordion body.</strong> Hello!!!
+  </BAccordionItem>
+</BAccordion>
+
+  </main>
+</template>
+
+<style scoped>
+    .BAccordian {
+        height: 100vh;
+        width: 100vw;
+    margin: 0;
+    }
+
+</style>>
