9752397471185c7f3dbea18715a1a7180b3e5231
Bryan Kongnyu <144625899+bryancods@users.noreply.github.com>
2024-04-18 18:26:52 +0000

    design fixes

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import { createBootstrap } from 'bootstrap-vue-next'
 // Add the necessary CSS
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
