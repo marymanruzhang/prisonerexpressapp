b362431682b4990549ed8aba056cb81f65156d62
Stephanie Dyer <157511938+sd625@users.noreply.github.com>
2024-04-18 03:50:30 +0000

    Routing is initialized

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
+
+// import fixtures from './fixtures.json'
+
+// main.js/ts
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
 import { createBootstrap } from 'bootstrap-vue-next'
-import fixtures from './fixtures.json';
+
+// Add the necessary CSS
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import './assets/main.css'
+
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
-
-app.use(createBootstrap())
 app.use(router)
-app.config.globalProperties.$fixtures = fixtures;
-app.config.globalProperties.baseDir = import.meta.env.BASE_URL;
+app.use(createBootstrap({ components: true, directives: true }))
+// app.config.globalProperties.fixtures = fixtures;
+// app.config.globalProperties.baseDir = import.meta.env.BASE_URL;
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+<h1>here is the home view </h1>
+
   </main>
 </template>
