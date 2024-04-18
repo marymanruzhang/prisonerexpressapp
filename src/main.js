8b904a313036a9b6361a99a8ff8e5755b6c84e50
Stephanie Dyer <157511938+sd625@users.noreply.github.com>
2024-04-18 03:52:03 +0000

    routing

---
+++ src/main.js
@@ -0,0 +1,22 @@
+
+// import fixtures from './fixtures.json'
+
+// main.js/ts
+import { createApp } from 'vue'
+import { createBootstrap } from 'bootstrap-vue-next'
+
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import './assets/main.css'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+app.use(router)
+app.use(createBootstrap({ components: true, directives: true }))
+// app.config.globalProperties.fixtures = fixtures;
+// app.config.globalProperties.baseDir = import.meta.env.BASE_URL;
+
+app.mount('#app')
