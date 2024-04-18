d00c8a92030a89efa3c1232335f7d38eb10cec90
marymanruzhang <156651928+marymanruzhang@users.noreply.github.com>
2024-04-18 15:10:20 +0000

    potential questions

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -7,14 +7,20 @@
 <h1>Frequently Asked Questions</h1>
 <p>Below are frequently asked questions</p>
 <BAccordion>
-  <BAccordionItem title="Accordion Item #1" visible>
-    <strong>This is the first item's accordion body.</strong> Hello!!
+  <BAccordionItem title="What does prisoner express do?" visible>
+    <strong>What does prisoner express do?</strong> Hello!!
   </BAccordionItem>
-  <BAccordionItem title="Accordion Item #2">
-    <strong>This is the second item's accordion body.</strong> Hello!!!
+  <BAccordionItem title="How to access current programs?">
+    <strong>How to access current programs?</strong> Hello!!!
   </BAccordionItem>
-  <BAccordionItem title="Accordion Item #3">
-    <strong>This is the third item's accordion body.</strong> Hello!!!
+  <BAccordionItem title="How to participate in a program you are interested in?">
+    <strong>How to visit your past works of works of other prisoners?</strong> Hello!!!
+  </BAccordionItem>
+  <BAccordionItem title="How to visit your past works of works of other prisoners?">
+    <strong>How to visit your past works of works of other prisoners?</strong> Hello!!!
+  </BAccordionItem>
+  <BAccordionItem title="How to submit works?">
+    <strong>How to submit works?</strong> Hello!!!
   </BAccordionItem>
 </BAccordion>
 
