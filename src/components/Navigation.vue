ff70680fbde11dbe05a648a5f381e00b37f7149f
Stephanie Dyer <157511938+sd625@users.noreply.github.com>
2024-04-19 17:46:08 +0000

    fixed sizing of nav bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,29 +1,61 @@
+
 <template>
-    <b-container class="fixed-bottom bottom-navbar">
+
+<b-container >
         <b-row>
-            <b-navbar class="list-unstyled" >
-                <b-col>
-                    <b-nav-item to= "/">
-                        <BImg src="./icons/mag_glass_icon.png" fluid />
-                    </b-nav-item>
-                </b-col>
-                <b-col>
-                    <b-nav-item>
-                        <BImg src="./icons/home_icon.png" fluid />
-                    </b-nav-item>
-                </b-col>
-                <b-col>
-                    <b-nav-item>
-                        <BImg src="./icons/info_icon.png" fluid />
-                    </b-nav-item>
-                </b-col>
-            </b-navbar>
+            <b-nav pills justified align="center" class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+                <b-nav-item href = "/" border-right>
+                    <b-row>
+                        <span class="material-symbols-outlined">home</span>
+                    </b-row>
+                    <b-row>
+                        <div>Home</div>
+                    </b-row>
+                </b-nav-item>
+                <b-nav-item>
+                    <b-row>
+                        <span class="material-symbols-outlined">bookmark</span>
+                    </b-row>
+                    <b-row>
+                        <div>Saved</div>
+                    </b-row>
+                </b-nav-item>
+                <b-nav-item>
+                    <b-row>
+                        <span class="material-symbols-outlined">school</span>
+                    </b-row>
+                    <b-row>
+                        <div>Programs</div>
+                    </b-row>
+                </b-nav-item>
+                <b-nav-item>
+                    <b-row>
+                        <span class="material-symbols-outlined">help</span>
+                    </b-row>
+                    <b-row>
+                        <div>FAQ</div>
+                    </b-row>
+                </b-nav-item>
+            </b-nav>
+
+
         </b-row>
-    </b-container>
+</b-container>
 </template>
 
-<style scoped>
-    .bottom-navbar {
-        max-height: 100px;
-    }
+<style >
+@import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap');
+
+
+template {
+    border-right: 1px solid black; /* Adds a black line to the right of each nav item */
+    color: black;
+}
+
+b-nav-item:last-child {
+    border-right: none; /* Removes the border from the last nav item */
+}
+
+
+
 </style>
