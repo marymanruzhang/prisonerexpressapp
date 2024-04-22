776e01747d7428ce401be0859c39743ea84f5483
Bryan Kongnyu <144625899+bryancods@users.noreply.github.com>
2024-04-22 03:14:10 +0000

    edits

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,55 +1,46 @@
-<template >
+<template>
+    <div class="container-fluid nav-container">
+        <b-row class="mx-0 px-0">
+            <b-nav pills justified fill class="w-100 navbar navbar-expand-lg navbar-light bg-light sticky-top">
+                <b-nav-item href="/">
+                    <b-row>
+                        <span class="material-symbols-outlined text-dark">home</span>
+                    </b-row>
+                    <b-row>
+                        <div class="text-dark">Home</div>
+                    </b-row>
+                </b-nav-item>
+                <b-nav-item>
+                    <b-row>
+                        <span class="material-symbols-outlined text-dark">bookmark</span>
+                    </b-row>
+                    <b-row>
+                        <div class="text-dark">Saved</div>
+                    </b-row>
+                </b-nav-item>
+                <b-nav-item href="/programs">
+                    <b-row>
+                        <span class="material-symbols-outlined text-dark">school</span>
+                    </b-row>
+                    <b-row>
+                        <div class="text-dark">Programs</div>
+                    </b-row>
+                </b-nav-item>
+                <b-nav-item>
+                    <b-row>
+                        <span class="material-symbols-outlined text-dark">help</span>
+                    </b-row>
+                    <b-row>
+                        <div class="text-dark">FAQ</div>
+                    </b-row>
+                </b-nav-item>
+            </b-nav>
+        </b-row>
+    </div>
+</template>
 
-    <b-container  class = "nav-container">
-            <b-row>
-                <b-nav pills justified align="center" class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-                    <b-nav-item href = "/">
-                        <b-row>
-                            <span class="material-symbols-outlined text-dark ">home</span>
-                        </b-row>
-                        <b-row>
-                            <div class="text-dark" >Home</div>
-                        </b-row>
-                    </b-nav-item>
-                    <b-nav-item>
-                        <b-row>
-                            <span class="material-symbols-outlined text-dark ">bookmark</span>
-                        </b-row>
-                        <b-row>
-                            <div class="text-dark" >Saved</div>
-                        </b-row>
-                    </b-nav-item>
-                    <b-nav-item  href = "/programs" >
-                        <b-row>
-                            <span class="material-symbols-outlined text-dark">school</span>
-                        </b-row>
-                        <b-row>
-                            <div class="text-dark" >Programs</div>
-                        </b-row>
-                    </b-nav-item>
-                    <b-nav-item >
-                        <b-row>
-                            <span class="material-symbols-outlined text-dark">help</span>
-                        </b-row>
-                        <b-row>
-                            <div class="text-dark" >FAQ</div>
-                        </b-row>
-                    </b-nav-item>
-                </b-nav>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap');
 
 
-            </b-row>
-    </b-container>
-    </template>
-
-    <style >
-    @import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap');
-
-    .nav-container {
-    position: fixed;
-    bottom: 0;
-
-    width:100%;
-}
-
-    </style>
+</style>
