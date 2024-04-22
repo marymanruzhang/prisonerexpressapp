a1597f8a5ff66aaa1a16ee1e2e1612b20722cdcb
marymanruzhang <156651928+marymanruzhang@users.noreply.github.com>
2024-04-22 23:35:50 +0000

    FAQ fixture accordion working

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <h1>Frequently Asked Questions</h1>
+    <h1>FAQ</h1>
     <p>Below are frequently asked questions</p>
     <BAccordion>
       <BAccordionItem v-for="(faq, index) in faqData" :key="index" :title="faq.title" :visible="faq.visible">
@@ -19,9 +19,18 @@ const faqItems = ref(faqData);
 
 
 <style scoped>
-.BAccordion {
-  height: 100vh;
-  width: 100vw;
-  margin: 0;
+.BAccordionItem {
+  width: 90vw; /* Adjust the width of the accordion items */
+  margin: 0 auto; /* Center the accordion items horizontally */
+  margin-bottom: 20px; /* Add margin between each accordion item */
+}
+
+h1 {
+  text-align: center;
+  padding-top: 3%;
+}
+
+p {
+  text-align: center;
 }
 </style>
